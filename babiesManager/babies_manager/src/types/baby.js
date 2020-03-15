c434687a0d66ffcd48ef40aa1eaf00e76eb36408
FR98 <ros18676@uvg.edu.gt>
2020-03-15 13:06:34 -0600

    Types

---
+++ babiesManager/babies_manager/src/types/baby.js
@@ -0,0 +1,2 @@
+export const BABY_ADDED = 'BABY_ADDED';
+export const BABY_SELECTED = 'BABY_SELECTED';
---
+++ babiesManager/babies_manager/src/types/event.js
@@ -0,0 +1,2 @@
+export const EVENT_ADDED = 'EVENT_ADDED';
+export const EVENT_DELETED = 'EVENT_DELETED';
